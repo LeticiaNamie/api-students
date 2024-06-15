b59209e537c7d24c9bf374cb4ba5907a615420d8
Leticia Namie <leticia.namie@hotmail.com>
2024-06-15 10:05:26 -0300

    feat: add echo routes

--- main.go
+++ main.go
@@ -16,11 +16,35 @@ func main() {
 
 	// Routes
 	e.GET("/students", getStudents)
+	e.POST("/student", createStudent)
+	e.GET("/student/:id", getStudent)
+	e.PUT("/student/:id", updateStudent)
+	e.DELETE("/student/:id", deleteStudent)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+// Handler
 func getStudents(c echo.Context) error {
-	return c.String(http.StatusOK, "List of all students:")
+	return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudent(c echo.Context) error {
+	return c.String(http.StatusOK, "Create student")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "Student: "+id)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "Update student: "+id)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "Delete student: "+id)
 }
