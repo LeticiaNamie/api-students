44af5f8a1f9746b97e32a687e0aca7cf62ff573c
Leticia Namie <leticia.namie@hotmail.com>
2024-06-19 19:58:49 -0300

    refactor: add swagger

--- api/api.go
+++ api/api.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"github.com/LeticiaNamie/api-students/db"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/LeticiaNamie/api-students/docs"
 )
 
 type API struct {
@@ -11,6 +14,12 @@ type API struct {
 	DB   *db.StudentHandler
 }
 
+// @title Student API
+// @version 1.0
+// @description This is a sample server Student API
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func NewServer() *API {
 	e := echo.New()
 
@@ -36,4 +45,5 @@ func (api *API) ConfigureRoutes() {
 	api.Echo.GET("/students/:id", api.getStudent)
 	api.Echo.PUT("/students/:id", api.updateStudent)
 	api.Echo.DELETE("/students/:id", api.deleteStudent)
+	api.Echo.GET("/swagger/*", echoSwagger.WrapHandler)
 }
--- api/handler.go
+++ api/handler.go
@@ -6,11 +6,22 @@ import (
 	"strconv"
 
 	"github.com/LeticiaNamie/api-students/schemas"
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
+// getStudents godoc
+//
+//	@Summary        Get a list of students
+//	@Description    Retrieve students details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Param          register path    int  false    "Registration"
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Router         /students [get]
 func (api *API) getStudents(c echo.Context) error {
 	students, err := api.DB.GetStudents()
 
@@ -38,6 +49,16 @@ func (api *API) getStudents(c echo.Context) error {
 	return c.JSON(http.StatusOK, listOfStudents)
 }
 
+// createStudent godoc
+//
+//	@Summary        Create student
+//	@Description    Create student
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        400
+//	@Router         /students [post]
 func (api *API) createStudent(c echo.Context) error {
 	studentReq := StudentRequest{}
 
@@ -67,6 +88,17 @@ func (api *API) createStudent(c echo.Context) error {
 	return c.JSON(http.StatusOK, student)
 }
 
+// getStudent godoc
+//
+//	@Summary        Get student by ID
+//	@Description    Retrieve student details using ID
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [get]
 func (api *API) getStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 
@@ -87,11 +119,33 @@ func (api *API) getStudent(c echo.Context) error {
 	return c.JSON(http.StatusOK, student)
 }
 
+// updateStudent godoc
+//
+//	@Summary        Update Student
+//	@Description    Update student details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [put]
 func (api *API) updateStudent(c echo.Context) error {
 	id := c.Param("id")
 	return c.String(http.StatusOK, "Update student: "+id)
 }
 
+// deleteStudent godoc
+//
+//	@Summary        Delete Student
+//	@Description    Delete student details
+//	@Tags           students
+//	@Accept         json
+//	@Produce        json
+//	@Success        200 {object} schemas.StudentResponse
+//	@Failure        404
+//	@Failure        500
+//	@Router         /students/{id} [delete]
 func (api *API) deleteStudent(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 
